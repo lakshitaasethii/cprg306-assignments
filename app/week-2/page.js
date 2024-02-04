e150bb7cbdcd7cf4e792afe7d8fb50d2c2eaed1d
Lakshitaasethii <lakshita.sethi@edu.sait.ca>
2024-02-04 01:07:52 -0700

    week-2

---
+++ app/week-2/page.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import StudentInfo from "./student-info";
+
+export default function Page() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-2xl font-bold">Shopping List</h1>
+        <StudentInfo />
+      </div>
+    </main>
+
+    // </div>
+  );
+}
---
+++ app/week-2/student-info.js
@@ -0,0 +1,17 @@
+"use client";
+
+import Link from "next/link";
+export default function () {
+  return (
+    <div className="">
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <h1>Lakshita Sethi</h1>
+      <Link
+        href="https://github.com/Lakshitaasethii/cprg306_assigments.git"
+        className="text-yellow-500 underline"
+      >
+        My Github Link
+      </Link>
+    </div>
+  );
+}

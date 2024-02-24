824dd1e9c8305b0c259cf32d07bc6d068db63cff
Lakshitaasethii <lakshita.sethi@edu.sait.ca>
2024-02-23 21:26:57 -0700

    small changes

--- app/week-5/item.js
+++ app/week-5/item.js
@@ -1,7 +1,7 @@
 const Item = ({ name, quantity, category }) => {
   return (
-    <li className="m-4 p-2 list-none bg-green-200 border text-green-950 w-1/4 rounded-lg">
-      <h3 className="text-2xl font-bold">{name}</h3>
+    <li className="m-4 p-2 list-none bg-gray-400 border text-gray-900 w-1/4 rounded-xl">
+      <h3 className="text-2xl font-bold capitalize">{name}</h3>
       <p className="mt-[0.5rem]">{`Buy ${quantity} in ${category} aisle`}</p>
     </li>
   );
--- app/week-5/page.js
+++ app/week-5/page.js
@@ -2,8 +2,8 @@ import ItemList from "./item-list";
 
 const Page = () => {
   return (
-    <main className="bg-green-950">
-      <h1 className="text-4xl font-bold text-green-200 p-4">Shopping List</h1>
+    <main className="bg-gray-900 py-4">
+      <h1 className="text-4xl font-bold text-gray-200 p-4">Shopping List</h1>
       <ItemList />
     </main>
   );
